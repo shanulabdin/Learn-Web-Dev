230ac9d4d3436f64b145fffda290e65a36066b65
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-20 14:11:18 +0500

    first commit

---
+++ accessible for all/script.js
@@ -0,0 +1,36 @@
+const navbar = document.getElementById('navbar');
+const openButton = document.getElementById('open-sidebar-button');
+
+const media = window.matchMedia('(width < 700px)');
+
+media.addEventListener('change', (e) => updateNavbar(e));
+
+function updateNavbar(e) {
+  const isMobile = e.matches;
+  console.log(isMobile);
+  if (isMobile) {
+    navbar.setAttribute('inert', '');
+  } else {
+    navbar.removeAttribute('inert');
+  }
+}
+
+function openSidebar() {
+  navbar.classList.add('show');
+  openButton.setAttribute('aria-expanded', 'true');
+  navbar.setAttribute('inert');
+}
+
+function closeSidebar() {
+  navbar.classList.remove('show');
+  openButton.setAttribute('aria-expanded', 'false');
+  navbar.setAttribute('inert', '');
+}
+
+const navLinks = document.querySelectorAll('#navbar a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeSidebar();
+  }); 
+});
+updateNavbar(media);
