a8a1e1f8b377cd82f379792c91017ae8a3d57c3b
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-13 21:17:07 +0500

    learning localStorage

---
+++ practice/localStorage.js
@@ -0,0 +1,16 @@
+// learning localStorage
+
+let key = prompt('Enter key you want to set');
+let value = prompt('Enter value you want to set');
+
+localStorage.setItem(key, value);
+
+console.log(`the value at ${key} is ${localStorage.getItem(key)}`);
+
+if(key == "red" || key == "blue"){
+  localStorage.removeItem(key)
+}
+
+if(key == 0){
+  localStorage.clear()
+}
